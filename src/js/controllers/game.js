3ab7636b8cc8825d3287fad7bba286e6fbbcab23
Jovan <jovanstojanovic@elfak.rs>
2020-04-20 20:12:34 +0200

    end-game-finish-timer-add

--- src/js/controllers/game.js
+++ src/js/controllers/game.js
@@ -12,8 +12,9 @@ class Game {
     }
 
     newGame(name1, name2, lap) {
-        this.lap = lap;
-
+        alert("New Game Start!")
+        this.lap = lap || 0;
+        this.time = this.lap;
         let player0 = document.querySelector(".player0");
         let player1 = document.querySelector(".player1");
         this.players = [new PlayerController('white', name1, player0), new PlayerController('black', name2, player1)];
@@ -24,7 +25,7 @@ class Game {
         this.toMove;
         this.k = 0;
         this.boardHandler();
-
+        this.timer = this.timerOn();
     }
     restartMove() {
         this.fromMove = undefined;
@@ -36,8 +37,10 @@ class Game {
         let index;
         this.activePlayer === 'white' ? index = 0 : index = 1;
         let res = this.board.moveFigure(this.fromMove, this.toMove, this.activePlayer);
-        // console.log(res);
         if (res.finish) {
+            this.timerOff();
+            this.lap = this.time;
+            this.timer = this.timerOn();
             if (res.endGame) {
                 alert("Game end!")
                 this.players[index].playerlose();
@@ -45,7 +48,10 @@ class Game {
                 this.players.forEach(el => {
                     el.clearAll();
                 })
-                this.newGame('Player 1', "Player 2");
+                this.timerOff()
+
+                this.newGame('Player 1', "Player 2", this.time);
+
                 return;
             }
             this.players[index].eat(res.fallFigure);
@@ -75,8 +81,31 @@ class Game {
         });
     }
 
+    timerOn() {
+        return setInterval(() => {
+            var distance = this.lap--;
+            document.querySelector("#timer").innerHTML = distance;
+            if (distance < 0) {
+                clearInterval(this.timer);
+                document.querySelector("#timer").innerHTML = "EXPIRED";
+                let index;
+                this.activePlayer === 'white' ? index = 0 : index = 1;
+                alert("Game end!")
+                this.players[index].playerlose();
+                boardHtml.innerHTML = "";
+                this.players.forEach(el => {
+                    el.clearAll();
+                })
+                this.newGame('Player 1', "Player 2", this.time);
+            }
+        }, 1000);
+    }
+    timerOff() {
+        clearInterval(this.timer);
+    }
+
 
 }
 
 
-const app = new Game("Player 1", "Player 2");
+const app = new Game("Player 1", "Player 2", 30);
